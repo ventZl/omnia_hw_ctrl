4d0e233637d27aa911571106fd10b7e42bc4eab3
Marek Behún <marek.behun@nic.cz>
2023-08-29 16:26:15 +0200

    platform/common: Move reset_to_address() from cpu.h to reset.h
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- src/platform/common/cpu.h
+++ src/platform/common/cpu.h
@@ -227,27 +227,4 @@ static __force_inline void mdelay(uint32_t ms)
 
 #undef MSEC_TO_LOOPS
 
-static __force_inline __noreturn void reset_to_address(uint32_t isr_vec_addr)
-{
-	__noreturn void (*new_reset_handler)(void);
-	uint32_t sp;
-
-	disable_irq();
-
-	/* get stack pointer from ISR vector */
-	sp = *(volatile uint32_t *)isr_vec_addr;
-
-	new_reset_handler = (void *)*(volatile uint32_t *)(isr_vec_addr + 4);
-
-	/* set stack pointer */
-	set_msp(sp);
-
-	/* instruction synchronization barrier to flush pipeline */
-	isb();
-
-	/* branch instead of call so that nothing is pushed to stack */
-	asm volatile("bx %0\n\t" : : "r" (new_reset_handler));
-	unreachable();
-}
-
 #endif /* CPU_H */
